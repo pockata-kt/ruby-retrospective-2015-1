e2570b1674a2f5fabf54b3d5b8f81671c4b6cd92
pockata-kt <ptodorov7311@yahoo.com>
2016-01-27 13:05:22 +0200

    Improve task01 solution to meet requirements

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,15 +1,12 @@
-def convert_to_bgn(price, currency)
-  rate = if currency == :usd
-    1.7408
-  elsif currency == :eur
-    1.9557
-  elsif currency == :gbp
-   2.6415
-  else
-    1
-  end
+RATES = {
+  usd: 1.7408,
+  eur: 1.9557,
+  gbp: 2.6415,
+  bgn: 1,
+}
 
-  (price*rate).round(2)
+def convert_to_bgn(price, currency)
+  (price * RATES[currency]).round(2)
 end
 
 def compare_prices(price_1, currency_1, price_2, currency_2)
