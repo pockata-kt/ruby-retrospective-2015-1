998c2af8d8ab9bd93ccaa1079968983b4e61f050
pockata-kt <ptodorov7311@yahoo.com>
2016-01-27 16:06:28 +0200

    Improve task02 solution to meet requirement

--- solutions/02.rb
+++ solutions/02.rb
@@ -4,15 +4,13 @@ def grow(snake, direction)
 end
 
 def move(snake, direction)
-  grow(snake, direction) - [snake[0]]
+  grow(snake, direction).drop(1)
 end
 
 def is_obstacle?(snake, position, dimensions)
-  if snake.include? position
-    true
-  elsif position[0] < 0 or position[0] >= dimensions[:width]
-    true
-  elsif position[1] < 0 or position[1] >= dimensions[:height]
+  x, y = position
+  if x < 0 or x >= dimensions[:width] or y < 0 or y >= dimensions[:height] or
+     snake.include? position
     true
   else
     false
@@ -31,11 +29,11 @@ def danger?(snake, direction, dimensions)
 end
 
 def new_food(food, snake, dimensions)
-  first = (0..dimensions[:width]-1).to_a.sample
-  second = (0..dimensions[:height]-1).to_a.sample
+  first = (0..dimensions[:width] - 1).to_a.sample
+  second = (0..dimensions[:height] - 1).to_a.sample
   if snake.include? [first, second] or food.include? [first, second]
     new_food(food, snake, dimensions)
   else
-    [first,second]
+    [first, second]
   end
 end
